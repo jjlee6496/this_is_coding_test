fcfa3117fb139676d3126ca3b58e2de596fb6336
jjlee6496 <ljj3499@gmail.com>
2023-06-02 00:42:01 +0900

    ⚡️ DFS와 BFS 완료!!

--- DFS_BFS/DFS와BFS.py
+++ DFS_BFS/DFS와BFS.py
@@ -3,20 +3,21 @@ from collections import deque
 
 input = sys.stdin.readline
 n, m, v = map(int, input().split())
-graph = [[] for _ in range(m+1)] # graph 초기화
+graph = [[] for _ in range(n+1)] # graph 초기화
 visited = [0]*(n+1) #방문여부
 
 for i in range(m):
     a, b = map(int, input().split())
-    graph[a] += [b]
-    graph[b] += [a]
+    graph[a] += [b] # graph[a].append(b)
+    graph[b] += [a] # graph[b].append(a)
 
-result1 = []
-result2 = []
+# result1 = []
+# result2 = []
 
 def dfs(i):
     visited[i] = 1
-    result1.append(i)
+    # result1.append(i)
+    print(i, end=' ')
     for x in sorted(graph[i]):
         if visited[x]==0:
             dfs(x)
@@ -27,13 +28,15 @@ q = deque([v])
 def bfs(i):
     while q:
         a = q.popleft()
-        result2.append(a)
+        # result2.append(a)
+        print(a, end=' ')
         for x in sorted(graph[a]):
             if visited[x] == 0:
                 q.append(x)
                 visited[x] = 1
+print()
 bfs(v)
-print(result1)
-print(result2)
+# print(result1)
+# print(result2)
     
     
